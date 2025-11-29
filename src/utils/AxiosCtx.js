c96832ff89da22d1a4a79bec84bff63943743b90
ezhil2233@gmail.com <ezhil>
2025-11-29 08:46:01 +0530

    Env property file added

--- src/utils/AxiosCtx.js
+++ src/utils/AxiosCtx.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosAPI = axios.create({
-    //baseURL: process.env.API_URL
-    baseURL: "http://localhost:8080/api"
+     baseURL: process.env.AGM_API_URL
+    //baseURL: "http://localhost:8080/api"
 });
 
